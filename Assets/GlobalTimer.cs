760ce56b1f6a05106462d85e238768c743a324c8
Rodrigomoize <116026830+Rodrigomoize@users.noreply.github.com>
2025-04-20 19:01:57 +0200

    Timer + Results scene

---
+++ Assets/GlobalTimer.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerGlobal : MonoBehaviour
+{
+    public float totalTime = 180f; 
+    public TMP_Text timerText;
+
+
+    private float timeLeft;
+
+    void Start()
+    {
+        timeLeft = totalTime;
+    }
+
+    void Update()
+    {
+
+        timeLeft -= Time.deltaTime;
+
+       
+        if (timeLeft < 0)
+            timeLeft = 0;
+
+        
+        int minutes = Mathf.FloorToInt(timeLeft / 60f);
+        int seconds = Mathf.FloorToInt(timeLeft % 60f);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        
+        if (timeLeft <= 0)
+        {
+            SceneManager.LoadScene("Results");
+        }
+    }
+}
